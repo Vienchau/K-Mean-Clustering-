6e13ce53a85716c6ca60b9529b1bc10e70f45a61
VienChau <Chautrivien2103@gmail.com>
2022-01-24 11:59:25 +0700

    final commit

--- KMC.py
+++ KMC.py
@@ -5,19 +5,19 @@ import pandas as pd
 import sklearn
 from sklearn import datasets
 from sklearn.cluster import KMeans
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder 
 from sklearn.preprocessing import LabelEncoder
 
-
+#Data reading
 Mall_Customers = pd.read_csv('Mall_Customers.csv')
 print(Mall_Customers.head())
 
+#Checking percent off data missing
 for col in Mall_Customers.columns:
     miss = Mall_Customers[col].isna().sum()
     miss_percent = miss/len(Mall_Customers.columns)
 print('Missing Data percent: %f ' %miss_percent)
 
+#split Data, chose columns: Genre, Annual Income, Spending score. Split Male and Female to analyze
 X = Mall_Customers.iloc[:, [1,3,4]].values
 X1 = Mall_Customers.iloc[:, [3,4]].values
 le = LabelEncoder()
@@ -44,6 +44,7 @@ Female = np.delete(Female, 0, 0)
 print('Number of Male: %i' %len(Male))
 print('Number of Female: %i' %len(Female))
 
+#Using Elbow to chosing K
 I = range(1,11)
 distortion_Male =[] 
 distortion_Female =[] 
@@ -68,30 +69,23 @@ plt.plot(I, distortion_Female)
 plt.ylabel('Distortion') 
 plt.title("Female Elbow K-Mean-Cluster")
 
+#chosing K = 5 in K mean Clustering 
 kmeans = KMeans(n_clusters=5, init = "k-means++", random_state = 42)
 y_means_Male = kmeans.fit_predict(Male)
 y_means_Female = kmeans.fit_predict(Female)
 
 
 plt.subplot(2,2,3)
-plt.scatter(Male[y_means_Male == 0, 0], Male[y_means_Male == 0, 1], s = 60, c = 'red', label = 'Cluster1')
-plt.scatter(Male[y_means_Male == 1, 0], Male[y_means_Male == 1, 1], s = 60, c = 'blue', label = 'Cluster2')
-plt.scatter(Male[y_means_Male == 2, 0], Male[y_means_Male == 2, 1], s = 60, c = 'green', label = 'Cluster3')
-plt.scatter(Male[y_means_Male == 3, 0], Male[y_means_Male == 3, 1], s = 60, c = 'violet', label = 'Cluster4')
-plt.scatter(Male[y_means_Male == 4, 0], Male[y_means_Male == 4, 1], s = 60, c = 'yellow', label = 'Cluster5') 
-plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 100, c = 'black', label = 'Centroids')
+plt.scatter(Male[:, 0], Male[:, 1], c=y_means_Male, s=50, cmap='viridis')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], c='black', s=200, alpha=0.5);
 plt.xlabel('Annual Income (k$)') 
 plt.ylabel('Spending Score (1-100)') 
 plt.title("Male K-Mean-Cluster")
 plt.legend() 
 
 plt.subplot(2,2,4)
-plt.scatter(Female[y_means_Female == 0, 0], Female[y_means_Female == 0, 1], s = 60, c = 'red', label = 'Cluster1')
-plt.scatter(Female[y_means_Female == 1, 0], Female[y_means_Female == 1, 1], s = 60, c = 'blue', label = 'Cluster2')
-plt.scatter(Female[y_means_Female == 2, 0], Female[y_means_Female == 2, 1], s = 60, c = 'green', label = 'Cluster3')
-plt.scatter(Female[y_means_Female == 3, 0], Female[y_means_Female == 3, 1], s = 60, c = 'violet', label = 'Cluster4')
-plt.scatter(Female[y_means_Female == 4, 0], Female[y_means_Female == 4, 1], s = 60, c = 'yellow', label = 'Cluster5') 
-plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 100, c = 'black', label = 'Centroids')
+plt.scatter(Female[:, 0], Female[:, 1], c=y_means_Female, s=50, cmap='viridis')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], c='black', s=200, alpha=0.5);
 plt.xlabel('Annual Income (k$)') 
 plt.ylabel('Spending Score (1-100)') 
 plt.title("Female K-Mean-Cluster")
